c0df79a9e61c786ae2ce88b7d3aef8cc6ab174ca
Lucas Brandão <71549267+correlucas@users.noreply.github.com>
2023-01-04 21:24:05 +0100

    check

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-const input = document.getElementById('input');
 const output = document.getElementById('output');
+const input = document.getElementById('input');
 const outputButton = document.querySelectorAll('[type="radio"]')
 const submitButton = document.getElementById('submitBtn');
 const submitAgainButton = document.getElementById('submit-again');
-
-let selected;
-
 const rating = document.getElementById('ratingOutput');
+const form = document.querySelector("form");
+const close = document.getElementById('closeModal');
+
+submitButton.addEventListener('click', () => {
+  if (rating.textContent === "") {
+    document.getElementById('modal').style.display = 'flex';
+  } else {
+    output.classList.remove('hidden');
+    input.classList.add('hidden');
+  }
+});
 
 outputButton.forEach((btn) => {
   btn.addEventListener("click", () => {
-    // submitBtn.disabled = false;
+    // submitButton.disabled = false;
     rating.innerHTML = btn.value;
   });
 });
 
+// open modal 
 
 
-submitButton.addEventListener('click', () => {
-  output.classList.remove('hidden');
+// reset form default
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault()
+  // output.classList.remove('hidden');
   input.classList.add('hidden');
-});
+})
 
 // comeback to the first form with RATE AGAIN 
 submitAgainButton.addEventListener('click', () => {
   output.classList.add('hidden');
   input.classList.remove('hidden');
+  rating.innerHTML = "";
+});
+
+// close modal
+close.addEventListener('click', () => {
+  document.getElementById('modal').style.display = 'none';
+});
+
+// close modal if click outside of it
+window.addEventListener('click', (event) => {
+  if (event.target == modal) {
+    document.getElementById('modal').style.display = 'none';
+  }
 });
